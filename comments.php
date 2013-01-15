67751c2e16a0697e7f7348b0b06e41eea9ec7cdc
Niklas Köhler <niklas.koehler@live.de>
2013-01-15 22:05:36 +0100

    fixed the layout of the userdata buttons inside the comments-template

--- comments.php
+++ comments.php
@@ -38,21 +38,18 @@
                 <?php if ( $user_ID ) : ?>
                     <p class="loginData"><?php printf(__('Angemeldet als %s'), '<a href="'.get_option('siteurl').'/wp-admin/profile.php">'.$user_identity.'</a>'); ?> <a href="<?php echo get_option('siteurl'); ?>/wp-login.php?action=logout" class="btn" title="<?php _e('Logout') ?>"><?php _e('Abmelden'); ?></a></p>
                 <?php else : ?>
-                    <input type="text" name="author" class="author" value="<?php echo $comment_author; ?>" size="22" tabindex="1" />
-                    <label for="author"><small><?php _e('Name'); ?><?php if ($req) _e('*'); ?></small></label><br />
-                                    
-                    <input type="text" name="email" class="email" value="<?php echo $comment_author_email; ?>" size="22" tabindex="2" />
-                    <label for="email"><small><?php _e('E-Mail-Adresse');?><?php if ($req) _e('*'); ?></small></label><br />
-                                    
-                    <input type="text" name="url" class="url" value="<?php echo $comment_author_url; ?>" size="22" tabindex="3" />
-                    <label for="url"><small><?php _e('Webseite'); ?></small></label><br />
-                    
-                    <small>*erforderliche Felder</small>
+                <div class="userdata">
+                    <input type="text" name="author" class="author" placeholder="Name" value="<?php echo $comment_author; ?>" tabindex="1" />
+                    <input type="text" name="email" class="email" placeholder="E-Mail" value="<?php echo $comment_author_email; ?>" tabindex="2" />       
+                    <input type="text" name="url" class="url" placeholder="Webseite" value="<?php echo $comment_author_url; ?>" tabindex="3" />
+                </div>
                 <?php endif; ?>
-                <textarea name="comment" class="commentBox" cols="100%" rows="10" tabindex="4"></textarea>
-                <small><?php printf(__('Sie können folgende Tags benutzen: %s'), allowed_tags()); ?></small>
-                <input class="btn" name="submit" type="submit" tabindex="5" value="<?php echo attribute_escape(__('Veröffentlichen')); ?>" />
-                <input type="hidden" name="comment_post_ID" value="<?php echo $id; ?>" />
+                <div>
+                    <textarea name="comment" cols="100%" rows="10" tabindex="4"></textarea>
+                    <small><?php printf(__('Sie können folgende Tags benutzen: %s'), allowed_tags()); ?></small>
+                    <input class="btn" name="submit" type="submit" tabindex="5" value="<?php echo attribute_escape(__('Veröffentlichen')); ?>" />
+                    <input type="hidden" name="comment_post_ID" value="<?php echo $id; ?>" />
+                </div>
                 <?php do_action('comment_form', $post->ID); ?>
             </form>
     </div>
